e5a8f14167adeb81800d5dab9f0161cd2a93ad7f
Nik68rus <Nik200x@mail.ru>
2019-08-17 14:19:12 +0300

    правка для корректной отрисовки иконок

--- src/components/event.js
+++ src/components/event.js
@@ -18,7 +18,7 @@ export const getEventMarkup = ({type, city, timeStart, duration, price, offers})
   <li class="trip-events__item">
     <div class="event">
       <div class="event__type">
-        <img class="event__type-icon" width="42" height="42" src="img/icons/${type}.png" alt="Event type icon">
+        <img class="event__type-icon" width="42" height="42" src="img/icons/${type.toLowerCase()}.png" alt="Event type icon">
       </div>
       <h3 class="event__title">${type} ${PLACE_TYPES.indexOf(type) < 0 ? `to` : `in`} ${city}</h3>
 
