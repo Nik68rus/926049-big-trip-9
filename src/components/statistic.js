c2eb0e3c84f03bd0e466b380e2689cd89b182f73
Nik68rus <Nik200x@mail.ru>
2019-09-09 21:31:01 +0300

    отрисовал статистику

--- src/components/statistic.js
+++ src/components/statistic.js
@@ -1,20 +1,16 @@
 import AbstractComponent from './abstarct-component';
 import Chart from 'chart.js';
 import ChartDataLabels from 'chartjs-plugin-datalabels';
-import {PLACE_TYPES, ACTION_TYPES} from '../constants';
+import {PLACE_TYPES, ACTION_TYPES, Time} from '../constants';
+
+const reducer = (accumulator, currentValue) => accumulator + currentValue;
+const priceMap = (curentEvent) => curentEvent.price;
+const durationMap = (curentEvent) => curentEvent.time.end - curentEvent.time.start;
 
 export default class Statistic extends AbstractComponent {
   constructor(events) {
     super();
     this._events = events;
-    this._moneyChart = null;
-  }
-
-  update(actualEvents) {
-    this._events = actualEvents;
-    this._updateMoneyChart();
-    this._updateTransportChart();
-    this._updateTimeChart();
   }
 
   getTemplate() {
@@ -39,18 +35,84 @@ export default class Statistic extends AbstractComponent {
 
   init() {
     const moneyCtx = document.querySelector(`.statistics__chart--money`);
-//    const transportCtx = document.querySelector(`.statistics__chart--transport`);
-//    const timeCtx = document.querySelector(`.statistics__chart--time`);
+    const transportCtx = document.querySelector(`.statistics__chart--transport`);
+    const timeCtx = document.querySelector(`.statistics__chart--time`);
 
-    const moneyChartData = this._getCosts();
+    const moneyChartData = this._getCosts(ACTION_TYPES.concat(PLACE_TYPES));
+    const transportChartData = this._getDurationRatios(this._getDurations(ACTION_TYPES));
+    const timeChartData = this._getDurationHours(this._getDurations(ACTION_TYPES.concat(PLACE_TYPES)));
 
-    this._moneyChart = new Chart(moneyCtx, {
+    this._moneyChart = new Chart(moneyCtx, this._makeChartConfig(`MONEY`, moneyChartData, (value) => `€` + value));
+    this._transportChart = new Chart(transportCtx, this._makeChartConfig(`TRANSPORT`, transportChartData, (value) => value + `x`));
+    this._timeChart = new Chart(timeCtx, this._makeChartConfig(`TIME`, timeChartData, (value) => value + `H`));
+  }
+
+  update(actualEvents) {
+    this._events = actualEvents;
+    this._updateChart(this._moneyChart, this._getCosts(ACTION_TYPES.concat(PLACE_TYPES)));
+    this._updateChart(this._transportChart, this._getDurationRatios(this._getDurations(ACTION_TYPES)));
+    this._updateChart(this._timeChart, this._getDurationHours(this._getDurations(ACTION_TYPES.concat(PLACE_TYPES))));
+  }
+
+  _getTypeData(type, dataMap) {
+    const fittingEvents = this._events.filter((curentEvent) => curentEvent.type === type);
+
+    if (fittingEvents.length === 0) {
+      return 0;
+    } else {
+      return fittingEvents.map(dataMap).reduce(reducer);
+    }
+  }
+
+  _getCosts(types) {
+    return types.map((type) => {
+      return {
+        name: type,
+        data: this._getTypeData(type, priceMap),
+      };
+    })
+    .filter((type) => type.data > 0)
+    .sort((a, b) => b.data - a.data);
+  }
+
+  _getDurations(types) {
+    return types.map((type) => {
+      return {
+        name: type,
+        data: this._getTypeData(type, durationMap),
+      };
+    })
+    .filter((type) => type.data > 0)
+    .sort((a, b) => b.data - a.data);
+  }
+
+  _getDurationRatios(durations) {
+    const minDuration = Math.min.apply(null, durations.map((type) => type.data));
+    return durations.map((type) => {
+      return {
+        name: type.name,
+        data: +(type.data / minDuration).toFixed(2),
+      };
+    });
+  }
+
+  _getDurationHours(durations) {
+    return durations.map((type) => {
+      return {
+        name: type.name,
+        data: Math.round(type.data / Time.HOUR),
+      };
+    });
+  }
+
+  _makeChartConfig(title, data, infoFormatter) {
+    return {
       plugins: [ChartDataLabels],
       type: `horizontalBar`,
       data: {
-        labels: moneyChartData.map((type) => type.name.toUpperCase()),
+        labels: data.map((type) => type.name.toUpperCase()),
         datasets: [{
-          data: moneyChartData.map((type) => type.cost),
+          data: data.map((type) => type.data),
           backgroundColor: `white`,
           borderColor: `grey`,
           borderWidth: 0,
@@ -60,6 +122,7 @@ export default class Statistic extends AbstractComponent {
       options: {
         plugins: {
           datalabels: {
+            formatter: infoFormatter,
             font: {
               size: 15
             },
@@ -70,19 +133,15 @@ export default class Statistic extends AbstractComponent {
         },
         scales: {
           yAxes: [{
-            ticks: {
-            },
             gridLines: {
               display: false,
               drawBorder: false
-            }
+            },
           }],
           xAxes: [{
             ticks: {
-              beginAtZero: true,
               display: false,
-              fontStyle: `bold`,
-              fontColor: `#000000`,
+              beginAtZero: true,
             },
             gridLines: {
               display: false,
@@ -96,56 +155,16 @@ export default class Statistic extends AbstractComponent {
         title: {
           display: true,
           position: `left`,
-          text: `MONEY`,
+          text: title,
           fontSize: 20,
         },
-        layout: {
-          padding: {
-            top: 10
-          }
-        },
-        tooltips: {
-          enabled: false
-        }
       }
-    });
+    };
   }
 
-  _getTypeCost(type) {
-    const reducer = (accumulator, currentValue) => accumulator + currentValue;
-    const fittingEvents = this._events.filter((curentEvent) => curentEvent.type === type);
-
-    if (fittingEvents.length === 0) {
-      return 0;
-    } else {
-      return fittingEvents.map((curentEvent) => curentEvent.price).reduce(reducer);
-    }
+  _updateChart(chart, newData) {
+    chart.data.labels = newData.map((type) => type.name.toUpperCase());
+    chart.data.datasets[0].data = newData.map((type) => type.data);
+    chart.update();
   }
-
-  _getCosts() {
-    return ACTION_TYPES.concat(PLACE_TYPES)
-    .map((type) => {
-      return {
-        name: type,
-        cost: this._getTypeCost(type),
-      };
-    })
-    .filter((type) => type.cost > 0);
-  }
-
-  _updateMoneyChart() {
-    const newMoneyChartData = this._getCosts();
-    this._moneyChart.data.labels = newMoneyChartData.map((type) => type.name.toUpperCase());
-    this._moneyChart.data.datasets[0].data = newMoneyChartData.map((type) => type.cost);
-    this._moneyChart.update();
-  }
-
-  _updateTransportChart() {
-
-  }
-
-  _updateTimeChart() {
-
-  }
-
 }
